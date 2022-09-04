8998f157f3479b395d62d899acc9cfd3e0215118
Kritika <kritistarsmind@gmail.com>
2022-09-04 13:09:30 +0530

    Annotation example

---
+++ src/com/dummyproject/AnnotationEx.java
@@ -0,0 +1,10 @@
+package com.dummyproject;
+
+public class AnnotationEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/dummyproject/MethodInfo.java
@@ -0,0 +1,5 @@
+package com.dummyproject;
+
+public class MethodInfo {
+
+}
